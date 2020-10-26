9c6b916e0854a3f0ec788c151f512c397b8388ad
denimendica <71275614+denimendica@users.noreply.github.com>
2020-10-26 09:19:02 +0100

    Add files via upload

---
+++ vjezba-varijabla/vjezba-varijabla/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba_varijabla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ocjena;
+            float brojOcjena = 0;
+            float zbrojOcjena = 0;
+            string uputa = "Upiši ocjenu: ";
+            string ispis = "Prosjek ocjena je: ";
+            float prosjek;
+
+            do
+            {
+                Console.Write(uputa);
+                ocjena=Convert.ToInt32(Console.ReadLine());
+                if (ocjena >= 1 && ocjena <= 5)
+                {
+                    
+                    brojOcjena++;
+                    zbrojOcjena += ocjena;
+
+                }else if (ocjena == 0)
+                {
+                    prosjek = zbrojOcjena / brojOcjena;
+                    Console.Write(ispis);
+                    Console.WriteLine(prosjek);
+                }
+                else
+                {
+                    Console.WriteLine("Molimo unesite ispravnu ocjenu (1-5)");
+                }
+
+            }
+            while (ocjena != 0);
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
